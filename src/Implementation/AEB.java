dac80f973c1b32f769f9c4e5b17882c02a351d6e
adamita <adamita22@gmail.com>
2015-11-11 20:14:07 +0100

    First

---
+++ src/Implementation/AEB.java
@@ -0,0 +1,18 @@
+package Implementation;
+import Interfaces.IRadarBase;
+public class AEB {
+	
+	IRadarBase Radar;
+	public AEB(IRadarBase radar)
+	{
+		
+	}
+	
+	private void Checking()
+	{
+		Thread.sleep(1000);
+	}
+	
+
+
+}
---
+++ src/Interfaces/IRadarBase.java
@@ -0,0 +1,12 @@
+package Interfaces;
+import java.awt.Point;
+import java.util.List;
+
+public interface IRadarBase
+{
+    void Signal();
+    List<Point> SetTriangle();
+    Point SetRadarPosition(Point carPosition);
+    Point GetNearestObject(List<Point> objects);
+    double RelativeSpeedInKPH(Point radarPos, Point nearestPos);
+}
